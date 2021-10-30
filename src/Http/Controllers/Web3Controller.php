a75aea7e5373f97768f653dfd1b78dbbaa4380a2
sawirricardo <sawirricardo@users.noreply.github.com>
2021-10-30 03:15:20 +0000

    Fix styling

--- src/Http/Controllers/Web3Controller.php
+++ src/Http/Controllers/Web3Controller.php
@@ -6,28 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Web3Controller
 {
-
-  public function store()
-  {
-    $data = request()->validate([
+    public function store()
+    {
+        $data = request()->validate([
       'address' => ['required', 'string', 'regex:/0x[a-fA-F0-9]{40}/m'],
       'signature' => ['required', 'string', 'regex:/^0x([A-Fa-f0-9]{130})$/'],
     ]);
 
-    $user = $this->getUserModel()->firstOrCreate([
+        $user = $this->getUserModel()->firstOrCreate([
       'account' => $data['address'],
     ]);
-  }
+    }
 
-  public function update()
-  {
-    request()->validate([
+    public function update()
+    {
+        request()->validate([
       'address' => ['required', 'string', 'regex:/0x[a-fA-F0-9]{40}/m'],
     ]);
-  }
+    }
 
-  protected function getUserModel(): Model
-  {
-    return app(config('auth.providers.users.model'));
-  }
+    protected function getUserModel(): Model
+    {
+        return app(config('auth.providers.users.model'));
+    }
 }
