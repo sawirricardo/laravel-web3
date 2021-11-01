50baf1361cf1e9f0797c92574757bc0aa04771c7
ricardosawir <sawir.ricardo@gmail.com>
2021-11-01 13:42:42 +0700

    fixed to update controller handle

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,6 @@ Route::prefix('_web3')
       ->middleware(['guest']);
     Route::resource('users', \Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class)
       ->middleware(['guest'])->only(['store']);
-    Route::delete('users/logout', \Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class)
+    Route::delete('users/logout', [\Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class, 'logout'])
       ->middleware(['auth']);
   });
