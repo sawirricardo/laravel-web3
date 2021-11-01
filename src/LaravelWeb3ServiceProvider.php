74d2404e6651b26a77abb89598d6082266782c7e
sawirricardo <sawirricardo@users.noreply.github.com>
2021-11-01 09:09:15 +0000

    Fix styling

--- src/LaravelWeb3ServiceProvider.php
+++ src/LaravelWeb3ServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Sawirricardo\LaravelWeb3;
 
 use Sawirricardo\LaravelWeb3\Commands\LaravelWeb3Command;
-use Sawirricardo\LaravelWeb3\LaravelWeb3Scripts;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
