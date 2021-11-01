e2ccfb696c74e01f42baf95e84df92b228e438b4
ricardosawir <sawir.ricardo@gmail.com>
2021-11-01 11:36:42 +0700

    added destroy route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('_web3')
   ->group(function () {
-    Route::get('users/signature', [\Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class, 'signature']);
-    Route::resource('users', \Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class)->only(['store']);
+    Route::get('users/signature', [\Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class, 'signature'])
+      ->middleware(['guest']);
+    Route::resource('users', \Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class)
+      ->middleware(['guest'])->only(['store']);
+    Route::resource('users', \Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class)
+      ->middleware(['auth'])->only(['destroy']);
   });
