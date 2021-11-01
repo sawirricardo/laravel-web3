db517332d85512ce39ea64e1762a779e530349da
ricardosawir <sawir.ricardo@gmail.com>
2021-11-01 11:04:08 +0700

    authenticate signature

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('_web3')
   ->group(function () {
+    Route::get('users/signature', [\Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class, 'signature']);
     Route::resource('users', \Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class)->only(['store']);
   });
--- src/Http/Controllers/Web3Controller.php
+++ src/Http/Controllers/Web3Controller.php
@@ -3,30 +3,66 @@
 namespace Sawirricardo\LaravelWeb3\Http\Controllers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 
 class Web3Controller
 {
-    public function store()
-    {
-        $data = request()->validate([
+  public function signature()
+  {
+    $nonce = Str::random();
+    request()->session()->put('nonce', $nonce);
+    return ['message' => $this->generateSignatureMessage($nonce)];
+  }
+
+  public function store()
+  {
+    $data = request()->validate([
       'address' => ['required', 'string', 'regex:/0x[a-fA-F0-9]{40}/m'],
       'signature' => ['required', 'string', 'regex:/^0x([A-Fa-f0-9]{130})$/'],
     ]);
 
-        $user = $this->getUserModel()->firstOrCreate([
+    if ($this->verifySignature(request()->session()->pull('nonce'), $data['signature'], $data['address'])) {
+      throw ValidationException::withMessages(['signature' => 'Signature verification failed.']);
+    }
+
+    $user = $this->getUserModel()->firstOrCreate([
       'account' => $data['address'],
     ]);
+
+    if (is_null(request()->user())) {
+      Auth::login($user);
     }
 
-    public function update()
-    {
-        request()->validate([
-      'address' => ['required', 'string', 'regex:/0x[a-fA-F0-9]{40}/m'],
-    ]);
-    }
+    return ['message' => 'Successfully logged in'];
+  }
 
-    protected function getUserModel(): Model
-    {
-        return app(config('auth.providers.users.model'));
-    }
+  protected function getUserModel(): Model
+  {
+    return app(config('auth.providers.users.model'));
+  }
+
+  protected function generateSignatureMessage($nonce)
+  {
+    $appName = config('app.name');
+    return "You are logging to {$appName}. Nonce: $nonce";
+  }
+
+  protected function verifySignature($nonce, $signature, $address)
+  {
+    $message = $this->generateSignatureMessage($nonce);
+    $msglen = strlen($message);
+    $hash   = \kornrunner\Keccak::hash("\x19Ethereum Signed Message:\n{$msglen}{$message}", 256);
+    $sign   = ["r" => substr($signature, 2, 64), "s" => substr($signature, 66, 64)];
+    $recid  = ord(hex2bin(substr($signature, 130, 2))) - 27;
+    if ($recid != ($recid & 1)) return false;
+    $pubkey = (new \Elliptic\EC('secp256k1'))->recoverPubKey($hash, $sign, $recid);
+    return hash_equals($address, $this->pubKeyToAddress($pubkey));
+  }
+
+  protected function pubKeyToAddress($pubkey)
+  {
+    return "0x" . substr(\kornrunner\Keccak::hash(substr(hex2bin($pubkey->encode("hex")), 1), 256), 24);
+  }
 }
