7a46d58f46e90949485e07bdeac5e29eb5a1429e
ricardosawir <sawir.ricardo@gmail.com>
2021-11-01 14:40:59 +0700

    removed guest middleware

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('_web3')
   ->middleware(['web'])
   ->group(function () {
-    Route::get('users/signature', [\Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class, 'signature'])
-      ->middleware(['guest']);
-    Route::post('users', [\Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class, 'store'])
-      ->middleware(['guest']);
+    Route::get('users/signature', [\Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class, 'signature']);
+    Route::post('users', [\Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class, 'store']);
     Route::delete('users/logout', [\Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class, 'logout'])
       ->middleware(['auth']);
   });
