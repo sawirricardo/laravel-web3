b560508c73bd354e2236d6551f5ca92ea800fb7e
ricardosawir <sawir.ricardo@gmail.com>
2021-11-01 13:19:11 +0700

    updated middleware web

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('_web3')
+  ->middleware(['web'])
   ->group(function () {
     Route::get('users/signature', [\Sawirricardo\LaravelWeb3\Http\Controllers\Web3Controller::class, 'signature'])
       ->middleware(['guest']);
